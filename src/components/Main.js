56010898fd21b77d205c046fd9077657244c3dd2
Serda Simus <iamserda@gmail.com>
2021-08-24 19:53:15 -0400

    minor fixes: React Effect Hooks

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Presentation from "./Presentation";
 import getData from "../AppBrain/GetData";
 import updateResults from "../AppBrain/GetResults";
